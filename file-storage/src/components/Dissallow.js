af1952015029f88fcab427f54425d2b2673665e6
Moneshsoni <sonimonesh115@gmail.com>
2023-05-31 13:47:18 +0530

    add the dissallow functionality

--- file-storage/src/components/Dissallow.js
+++ file-storage/src/components/Dissallow.js
@@ -1,21 +1,30 @@
-// import { useState } from "react";
-const Dissallow = ({ account, contract }) => {
+import "./Dissallow.css";
+import "./Modal.css";
+const Dissallow = ({ contract }) => {
+   
     const disallow = async () => {
-        contract.disallow(account);
-        console.log("working fine");
+        
+        const address = document.querySelector("#selectaddress").value;
+        try {
+            if (address) {
+              await contract.disallow(address);
+
+            }else{
+                alert("Please enter the address to disallow");
+            }
+          } catch (e) {
+            alert("You don't have put address",e);
+          }
       };
     return (
-      <>
-        <div className="body">
-            <input
-              type="text"
-              className="address"
-              placeholder="Enter Dissallow Address"
-            ></input>
-            <button className="center button" onClick={() => disallow()}>
-              Disallow
+      <>    
+        <input id = "selectaddress"
+            type="text"
+            className="address"
+            placeholder="Enter Dissallow Address"
+        ></input>
+        <button  className="center button" onClick={() => disallow()}>Disallow
             </button>
-        </div>
       </>
     );
   };
