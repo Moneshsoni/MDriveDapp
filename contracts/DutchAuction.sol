0f73da3ca4c19b10f6c26816204e071106c44fdf
Moneshsoni <sonimonesh115@gmail.com>
2023-06-15 11:46:26 +0530

    add the dutch auction

---
+++ contracts/DutchAuction.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.8.9;
+interface IERC721{
+    function transferFrom(address _from, address _to,uint _nftId)external;
+
+}
+
+contract DutchAuction{
+    uint private constant DURATION = 1 days;
+    IERC721 public immutable nft;
+    uint public immutable nftId;
+
+    address payable public immutable seller;
+    uint public immutable startingPrice;
+    uint public immutable startAt;
+    uint public immutable expiresAt;
+    uint public immutable discountRate;
+
+    constructor(uint _startingPrice,uint _discountRate, address _nft,uint _nftId){
+        seller = payable(msg.sender);
+        startingPrice = _startingPrice;
+        startAt = block.timestamp;
+        expiresAt = block.timestamp + DURATION;
+        discountRate = _discountRate;
+        require(_startingPrice >= _discountRate * DURATION ,"starting Price < min");
+        nft = IERC721(_nft);
+        nftId = _nftId;
+    }
+
+    function getPrice()public view returns(uint){
+        uint timeElapsed = block.timestamp - startAt;
+        uint discount = discountRate * timeElapsed;
+        return startingPrice - discount;
+    }
+
+
+    function buy() external payable{
+        require(block.timestamp < expiresAt, "auction expired");
+        uint price =  getPrice();
+        require(msg.value >= price,"Eth < price");
+        nft.transferFrom(seller, msg.sender, nftId);
+
+        uint refund = msg.value - price;
+        if(refund > 0){
+            payable(msg.sender).transfer(refund);
+        }
+        selfdestruct(seller);
+    }
+}
